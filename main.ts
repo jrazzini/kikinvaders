3494a7035171b4e70c5658a8cd2a94a2bfb45707
jrazzini <38938271+jrazzini@users.noreply.github.com>
2022-07-11 20:19:47 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once);
-basic.pause(3000);
+//music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once);
+//basic.pause(3000);
 // initialisation des variables
 let enemyY = 0;
 let random2 = 0;
@@ -10,6 +10,7 @@ let enemy = game.createSprite(5, 5);
 enemy.set(LedSpriteProperty.Brightness, 0);
 // commencement du score
 game.setScore(0);
+let countDown = 0;
 
 basic.forever(function () {
     let count = 0;
@@ -30,9 +31,22 @@ basic.forever(function () {
         enemyY = enemy.get(LedSpriteProperty.Y);
         let destroy = !enemy.isDeleted();
         if (enemyY == 4 && destroy) {
-            music.playMelody("C5 B A G F E D C ", 700);
-            music.playMelody("C5 B A G F E D C ", 700);
-            game.gameOver();
+            countDown++;
+            music.playMelody("C - F - B - A - ", 800);
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `,1000);
+                enemy.delete();
+            if (countDown == 3){
+                music.playMelody("C5 B A G F E D C ", 700);
+                music.playMelody("C5 B A G F E D C ", 700);
+                game.gameOver();
+            }
+            
         }
         random2 = randint(-1, 1);
         random = randint(0, 5);
